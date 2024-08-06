3c619090e049e5292e6e48f449f661c20f8f8c57
Hila_Levy <60661702+PenguinHila@users.noreply.github.com>
2024-08-07 02:28:54 +0300

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -8,12 +8,7 @@ from selenium.webdriver.common.by import By
 
 def test_scores_service():
     chrome_options = Options()
-
-    # Optionally add any necessary options here
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
-
+    
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
